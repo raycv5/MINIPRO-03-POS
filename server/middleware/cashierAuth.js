1ec799f1e7a621c854c7f9aba335844ccf1113c6
Agnaar <143308939+Agnaar@users.noreply.github.com>
2023-11-13 17:02:36 +0700

    add cashier and admin controller (#5)
    Co-authored-by: raycv5 <rayhanaw612@gmail.com>

---
+++ server/middleware/cashierAuth.js
@@ -0,0 +1,41 @@
+const jwt = require('jsonwebtoken');
+const { Cashier } = require('../models'); // Sesuaikan dengan model Cashier Anda
+
+const verifyAndFindCashierById = async (req, res, next) => {
+    try {
+        let token = req.headers.authorization;
+        if (!token) {
+            return res.status(401).json({
+                message: 'Token kosong'
+            });
+        }
+        token = token.split(' ')[1];
+
+        const decodedToken = jwt.verify(token, 'Jcwd0208');
+
+        const cashierId = decodedToken.id;
+
+        const cashier = await Cashier.findOne({
+            where: {
+                id: cashierId
+            }
+        });
+
+        if (!cashier) {
+            return res.status(400).json({
+                message: 'Kasir tidak ditemukan'
+            });
+        }
+
+        req.cashier = cashier;
+
+        next();
+    } catch (error) {
+        console.error(error);
+        res.status(401).json({
+            message: 'Token tidak valid'
+        });
+    }
+};
+
+module.exports = verifyAndFindCashierById;
---
+++ server/middleware/multer.js
@@ -0,0 +1,28 @@
+const multer =require('multer')
+
+module.exports= {
+    multerUpload: () => {
+        const storage = multer.diskStorage({
+            destination: (req, file, cb) => {
+                cb(null, './public')
+            },
+
+            filename: (req, file, cb) => {
+                cb(null, 'PIMG' + '-' + Date.now() + Math.round(Math.random() * 10000) + '.' + file.mimetype.split('/') [1])
+            }
+        })
+
+        const fileFilter = (req, file, cb) => {
+            const exFilter = ['jpg', 'jpeg', 'png', 'webp']
+            const checkExt = exFilter.includes(file.mimetype.split('/')[1].toLowerCase())
+
+            if (checkExt) {
+                cb(null, true)
+            }else{
+                cb(new error('file format not match'))
+            }
+         }
+
+         return multer({storage, fileFilter})
+        }
+    }
