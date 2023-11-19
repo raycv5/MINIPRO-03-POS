eaa6db9f72ecd35785768584f146e8289c1ae6e5
raycv5 <rayhanaw612@gmail.com>
2023-11-20 02:05:05 +0700

    Refactor cart components

--- client/src/components/menu/Cart.jsx
+++ client/src/components/menu/Cart.jsx
@@ -5,7 +5,6 @@ import {
   Flex,
   Grid,
   GridItem,
-  Image,
   Input,
   Modal,
   ModalBody,
@@ -14,17 +13,14 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
-  Radio,
-  RadioGroup,
-  Stack,
   Text,
   useDisclosure,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
-import { AiFillMinusCircle, AiFillPlusCircle } from "react-icons/ai";
-import { BiSolidTrashAlt } from "react-icons/bi";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import CartBottom from "./CartBottom";
+import CartItem from "./CartItem";
 
 function Cart({ getProducts, getCarts, cart }) {
   const [payment, setPayment] = useState("");
@@ -110,168 +106,20 @@ function Cart({ getProducts, getCarts, cart }) {
           <Text fontWeight="bold" fontSize="2xl" marginBottom="20px">
             Current Order
           </Text>
-          <Box
-            overflowY="auto"
-            height="60vh"
-            sx={{
-              "&::-webkit-scrollbar": {
-                width: "4px",
-              },
-              "&::-webkit-scrollbar-track": {
-                borderRadius: "10px",
-                bg: "#f1f1f1",
-              },
-              "&::-webkit-scrollbar-thumb": {
-                borderRadius: "10px",
-                bg: "#bfbfbf",
-              },
-            }}
-          >
-            {cart?.items?.map((item) => {
-              return (
-                <Flex
-                  key={item.id}
-                  justifyContent="space-between"
-                  bgColor="blackAlpha.200"
-                  border="1px"
-                  borderColor="white"
-                  padding="2%"
-                  rounded="xl"
-                  marginTop="10px"
-                  _hover={{
-                    bgColor: "orange.100",
-                    borderColor: "orange",
-                  }}
-                >
-                  <Stack direction="row">
-                    <Image
-                      src={`http://localhost:2000/${item.Product.image}`}
-                      boxSize="80px"
-                      objectFit="cover"
-                      rounded="xl"
-                    />
-                    <Flex flexDirection="column">
-                      <Text fontWeight="bold">{item.Product.name}</Text>
-                      <Text fontWeight="semibold">
-                        {item?.Product?.price.toLocaleString("id-ID", {
-                          style: "currency",
-                          currency: "IDR",
-                        })}
-                      </Text>
-                      <Text>
-                        x{" "}
-                        <Text as={"span"} fontWeight={"bold"}>
-                          {item.quantity}
-                        </Text>
-                      </Text>
-                    </Flex>
-                  </Stack>
-                  <Flex
-                    flexDirection="column"
-                    justifyContent="space-between"
-                    alignItems="end"
-                  >
-                    <Stack direction="row" alignItems="center">
-                      {/* ----- DECREMENT ----- */}
-                      <Text
-                        _hover={{ color: "orange" }}
-                        cursor="pointer"
-                        onClick={() => handleDecrement(item.id)}
-                      >
-                        <AiFillMinusCircle size="20" />
-                      </Text>
-                      <Text>{item.quantity}</Text>
-                      {/* ----- INCREMENT ----- */}
-                      <Text
-                        _hover={{ color: "orange" }}
-                        cursor="pointer"
-                        onClick={() => handleIncrement(item.id)}
-                      >
-                        <AiFillPlusCircle size="20" />
-                      </Text>
-                    </Stack>
-                    {/* ----- DELETE ----- */}
-                    <Text
-                      cursor="pointer"
-                      _hover={{ color: "red.600" }}
-                      onClick={() => handleDelete(item.id)}
-                    >
-                      <BiSolidTrashAlt size="20" />
-                    </Text>
-                  </Flex>
-                </Flex>
-              );
-            })}
-          </Box>
+          <CartItem
+            cart={cart}
+            handleDecrement={handleDecrement}
+            handleIncrement={handleIncrement}
+            handleDelete={handleDelete}
+          />
         </Box>
       </GridItem>
       <GridItem>
-        <Flex
-          area={"bottom"}
-          bgColor="blackAlpha.200"
-          rounded="xl"
-          height="100%"
-          padding="5%"
-          flexDirection="column"
-          justifyContent="space-between"
-        >
-          <Box>
-            <Flex justifyContent="space-between">
-              <Text>
-                Items (
-                <Text as={"span"} fontWeight="semibold">
-                  {cart?.items?.length}
-                </Text>
-                )
-              </Text>
-              <Text fontWeight="semibold">
-                {cart?.total_price?.toLocaleString("id-ID", {
-                  style: "currency",
-                  currency: "IDR",
-                })}
-              </Text>
-            </Flex>
-            <Flex justifyContent="space-between">
-              <Text>Discount</Text>
-              <Text fontWeight="semibold">0</Text>
-            </Flex>
-            <Flex
-              fontWeight="bold"
-              justifyContent="space-between"
-              borderY="1px"
-              paddingY="6px"
-              marginY="6px"
-            >
-              <Text>Total</Text>
-              <Text>
-                {cart?.total_price?.toLocaleString("id-ID", {
-                  style: "currency",
-                  currency: "IDR",
-                })}
-              </Text>
-            </Flex>
-          </Box>
-          <Box>
-            <Text fontWeight="bold">Payment Method</Text>
-            <RadioGroup onChange={handlePayment}>
-              <Flex justifyContent="space-between" padding="2%">
-                <Radio value="1">Cash</Radio>
-                <Radio value="2">Debit</Radio>
-                <Radio value="3">QRIS</Radio>
-              </Flex>
-            </RadioGroup>
-          </Box>
-          <Box textAlign="center">
-            <Button
-              width="100%"
-              bgColor="orange"
-              _hover={{ bgColor: "orange.200" }}
-              onClick={handleSubmit}
-            >
-              Checkout
-            </Button>
-          </Box>
-        </Flex>
+        <CartBottom
+          cart={cart}
+          handlePayment={handlePayment}
+          handleSubmit={handleSubmit}
+        />
       </GridItem>
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
