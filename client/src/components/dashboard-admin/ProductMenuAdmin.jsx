66583a06c631d8b9bbc547a9df560053f1bce5f7
raycv5 <rayhanaw612@gmail.com>
2023-11-20 10:02:48 +0700

    Fix errors (#21)

--- client/src/components/dashboard-admin/ProductMenuAdmin.jsx
+++ client/src/components/dashboard-admin/ProductMenuAdmin.jsx
@@ -4,33 +4,32 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Navbar from "../menu/Navbar";
 import { Flex } from "@chakra-ui/react";
-import { ProductCategory } from "./ProductCategory";
+// import { ProductCategory } from "./ProductCategory";
 
-
-import { Box, Flex, Grid, GridItem, Image, Text } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Image, Text } from "@chakra-ui/react";
 import MenuCard from "../menu/MenuCard";
 
 export const ProductMenuAdmin = ({
-   product,
-   getProducts,
-   getCarts,
-   setCategory,
-   categoryId,
+  product,
+  getProducts,
+  getCarts,
+  setCategory,
+  categoryId,
 }) => {
-   console.log(product)
-   return (
-      <>
-         <Navbar
-            setCategory={setCategory}
-            getProducts={getProducts}
-            categoryId={categoryId}
-            route={"dashboard-admin"}
-         />
-         <MenuCard
-            product={product}
-            getProducts={getProducts}
-            categoryId={categoryId}
-         />
-      </>
-   );
+  console.log(product);
+  return (
+    <>
+      <Navbar
+        setCategory={setCategory}
+        getProducts={getProducts}
+        categoryId={categoryId}
+        route={"dashboard-admin"}
+      />
+      <MenuCard
+        product={product}
+        getProducts={getProducts}
+        categoryId={categoryId}
+      />
+    </>
+  );
 };
